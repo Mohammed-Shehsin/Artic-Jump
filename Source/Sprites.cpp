5e676cb8b33e1782c1a1aee2f8c79518ee1487a6
Mohammed Shehsin Thamarachalil Abdulresak <115590581+Mohammed-Shehsin@users.noreply.github.com>
2023-05-21 10:31:33 -0700

    -Class Sprite&&Player
    -Alternative part

--- Source/Sprites.cpp
+++ Source/Sprites.cpp
@@ -24,6 +24,34 @@ public:
 
     }
 };
+
+
+// //Parent Class Sprite
+
+// class Sprites : public sf::Sprite
+// {
+// public:
+//     Sprites() {}
+    
+//     void setTexture(const std::string& texturePath)
+//     {
+//         if (!texture.loadFromFile(texturePath))
+//         {
+//             std::cerr << "Could not load texture" << std::endl;
+//             return 1;
+//         }
+        
+//         // Set the texture for the sprite
+//         this->sf::Sprite::setTexture(texture);
+//     }
+    
+// protected:
+//     sf::Texture texture;
+// };
+
+
+
+
 class Player: public Sprites{
 protected:
     sf::Texture texture;
@@ -60,3 +88,79 @@ public:
     }
 
 };
+
+// //Player class
+
+// class Player : public Sprites
+// {
+// public:
+//     Player(const std::string& texturePath) : Sprites()
+//     {
+//         setTexture(texturePath);
+//         speed = 5.0f;
+//         jumpHeight = 10.0f;
+//         isJumping = false;
+//         jumpKeyPressed = false;
+//     }
+    
+//     void moveInDirection()
+//     {
+//         float direction = 0.0f;
+//         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+//         {
+//             direction -= 1.0f;
+//         }
+//         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+//         {
+//             direction += 1.0f;
+//         }
+//         sf::Vector2f movement(speed * direction, 0.0f);
+//         this->move(movement);
+//     }
+    
+//     bool collision(const sf::Sprite& other)
+//     {
+//         return this->getGlobalBounds().intersects(other.getGlobalBounds());
+//     }
+    
+//     void jump()
+//     {
+//         if (!isJumping && sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+//         {
+//             isJumping = true;
+//             jumpDistance = jumpHeight;
+//         }
+//     }
+    
+//     void update()
+//     {
+//         moveInDirection();
+        
+//         if (isJumping)
+//         {
+//             float yMovement = -0.5f * jumpDistance;
+//             sf::Vector2f movement(0.0f, yMovement);
+//             this->move(movement);
+//             jumpDistance -= 1.0f;
+            
+//             if (jumpDistance <= 0.0f)
+//             {
+//                 isJumping = false;
+//             }
+//         }
+//     }
+    
+// protected:
+//     float speed;
+//     float jumpHeight;
+//     bool isJumping;
+//     float jumpDistance;
+//     bool jumpKeyPressed;
+// };
+
+
+
+
+
+
+
