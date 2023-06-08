99d7755ef1b747bc0d51a3cb1e457ee09deadf49
Mohammed Shehsin Thamarachalil Abdulresak <115590581+Mohammed-Shehsin@users.noreply.github.com>
2023-06-08 11:49:38 +0200

    set collusion against the window ~Gold class

--- Source/Sprites.cpp
+++ Source/Sprites.cpp
@@ -332,4 +332,35 @@ public:
         }
         return false;
     }
+     void update(const sf::RenderWindow& window){
+        // Check collision with the window boundaries
+        sf::FloatRect goldBounds = getGlobalBounds();
+        sf::FloatRect windowBounds(0.0f, 0.0f, window.getSize().x, window.getSize().y);
+
+        if (!windowBounds.contains(goldBounds.left, goldBounds.top)
+            || !windowBounds.contains(goldBounds.left + goldBounds.width, goldBounds.top)
+            || !windowBounds.contains(goldBounds.left,goldBounds.top + goldBounds.height)
+            || !windowBounds.contains(goldBounds.left + goldBounds.width, goldBounds.top + goldBounds.height))
+        {
+            // Adjust player position to stay within the window
+            sf::Vector2f newPosition;
+
+            if (goldBounds.left < 0.0f)
+                newPosition.x = 0.0f;
+            else if (goldBounds.left + goldBounds.width > windowBounds.width)
+                newPosition.x = windowBounds.width - goldBounds.width;
+            else
+                newPosition.x = getPosition().x;
+
+            if (goldBounds.top < 0.0f)
+                newPosition.y = 0.0f;
+            else if (goldBounds.top +goldBounds.height > windowBounds.height)
+                newPosition.y = windowBounds.height - goldBounds.height;
+            else
+                newPosition.y = getPosition().y;
+
+            setPosition(newPosition);
+        }
+
+
 };
