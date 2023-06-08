ace6045cd99aa46451bcd70f248ace4a4dfb9784
Mohammed Shehsin Thamarachalil Abdulresak <115590581+Mohammed-Shehsin@users.noreply.github.com>
2023-06-08 11:47:41 +0200

    set collusion against window~Heart class

--- Source/Sprites.cpp
+++ Source/Sprites.cpp
@@ -284,6 +284,36 @@ public:
         }
         return false;
     }
+     // update function to stay the heart inside the window
+     void update(const sf::RenderWindow& window){
+      // Check collision with the window boundaries
+      sf::FloatRect heartBounds = getGlobalBounds();
+      sf::FloatRect windowBounds(0.0f, 0.0f, window.getSize().x, window.getSize().y);
+
+      if (!windowBounds.contains(heartBounds.left, heartBounds.top)
+          || !windowBounds.contains(heartBounds.left + heartBounds.width, heartBounds.top)
+          || !windowBounds.contains(heartBounds.left,heartBounds.top + heartBounds.height)
+          || !windowBounds.contains(heartBounds.left + heartBounds.width, heartBounds.top + heartBounds.height))
+      {
+                // Adjust player position to stay within the window
+                sf::Vector2f newPosition;
+
+                if (heartBounds.left < 0.0f)
+                newPosition.x = 0.0f;
+                else if (heartBounds.left + heartBounds.width > windowBounds.width)
+                newPosition.x = windowBounds.width - heartBounds.width;
+                else
+                newPosition.x = getPosition().x;
+
+                if (heartBounds.top < 0.0f)
+                newPosition.y = 0.0f;
+                else if (heartBounds.top + heartBounds.height > windowBounds.height)
+                newPosition.y = windowBounds.height - heartBounds.height;
+                else
+                newPosition.y = getPosition().y;
+
+                setPosition(newPosition);
+      }
 };
 
 //Gold - Pickable
