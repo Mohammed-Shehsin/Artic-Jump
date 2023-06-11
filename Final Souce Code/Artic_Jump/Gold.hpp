ee51182f1bde07a35f251e0283ec2d6095997b62
Mohammed Shehsin Thamarachalil Abdulresak <115590581+Mohammed-Shehsin@users.noreply.github.com>
2023-06-11 02:23:44 +0200

    add collision()

--- Final Souce Code/Artic_Jump/Gold.hpp
+++ Final Souce Code/Artic_Jump/Gold.hpp
@@ -10,7 +10,8 @@ class Gold : public Pickable
 public:
     Gold(const std::string& texturePath);
     bool checkCollision(Player& player);
-    void update(const sf::RenderWindow& window);
+    void collision(const std::vector<std::unique_ptr<Immovable>>& platforms) ;
+    void update(const std::vector<std::unique_ptr<Immovable>>& platforms, const sf::RenderWindow& window);
 };
 
 #endif // GOLD_HPP
