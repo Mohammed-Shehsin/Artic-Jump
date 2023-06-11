c0618acc073909b7c3fa9f69999619ea73747e80
Mohammed Shehsin Thamarachalil Abdulresak <115590581+Mohammed-Shehsin@users.noreply.github.com>
2023-06-11 02:27:20 +0200

    Update Heart.hpp

--- Final Souce Code/Artic_Jump/Heart.hpp
+++ Final Souce Code/Artic_Jump/Heart.hpp
@@ -11,7 +11,7 @@ public:
     Heart(const std::string& texturePath);
     bool checkCollision(Player& player);
     void collision(const std::vector<std::unique_ptr<Immovable>>& platforms);
-    void update(const sf::RenderWindow& window);
+    void update(const std::vector<std::unique_ptr<Immovable>>& platforms,const sf::RenderWindow& window);
 };
 
 #endif // HEART_HPP
